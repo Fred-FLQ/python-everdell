ddfe316186bedf0dbfe2dd95399299b782055902
Fred - FLQ <fredflq@freds-macbook-air.home>
2023-10-17 13:51:31 +0200

    New Game class to handle game mechanics, incl. player switch

--- everdell.py
+++ everdell.py
@@ -16,10 +16,73 @@ with open("cards_deck.json") as cards_deck:
 #   Game Setup   #
 ##################
 
+# New class to handle game mechanics
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.active_player = self.player1
+
+        # Create players' town instances
+        self.player1_town = Town(player1, self)
+        self.player2_town = Town(player2, self)
+
+        self.action_menu()
+        self.process_user_input()
+    
+    def switch_player(self):
+        if self.active_player == self.player1:
+            self.active_player = self.player2
+        else:
+            self.active_player = self.player1
+
+    # Display possible player's actions
+    def action_menu(self):
+        print(f"\n{self.active_player}, what do you wanna do next?")
+        print("1. Display Town's Dashboard")
+        print("2. Display cards in hand")
+        print("3. Place a worker")
+    
+     # Process player's choice depending on active player
+    def process_user_input(self):
+        user_input = input(f"{self.active_player}, choose an action: ")
+        if user_input == "1":
+            if self.active_player == self.player1:
+                self.player1_town.town_dashboard()
+            else:
+                self.player2_town.town_dashboard()
+        elif user_input == "2":
+            if self.active_player == self.player1:
+                if len(self.player1_town.cards_in_hand) > 0:
+                    print(f"\n{self.active_player}, here are the cards you have in hand:")
+                    print(self.player1_town.cards_in_hand)
+                    self.player1_town.play_card(input(f"{self.active_player}, which card do you wanna play? "))
+                else:
+                    print(f"{self.active_player}, you don't have any cards in your hand.")
+                    self.action_menu()
+                    self.process_user_input()
+            if self.active_player == self.player2:
+                if len(self.player2_town.cards_in_hand) > 0:
+                    print(f"\n{self.active_player}, here are the cards you have in hand:")
+                    print(self.player2_town.cards_in_hand)
+                    self.player2_town.play_card(input(f"{self.active_player}, which card do you wanna play? "))
+                else:
+                    print(f"{self.active_player}, you don't have any cards in your hand.")
+                    self.action_menu()
+                    self.process_user_input()
+        elif user_input == "3":
+            self.place_worker()
+        else:
+            print(
+                "Your choice is not valid. Input the number of the action you wanna do."
+            )
+            self.action_menu()
+            self.process_user_input()
 
 class Town:
-    def __init__(self, name):
+    def __init__(self, name, game):
         self.name = name
+        self.game = game
         self.workers = 2
         self.total_points = 0
         self.constructions = []
@@ -41,13 +104,13 @@ class Town:
     # Dedicated method to display Town's Dashboard
     def town_dashboard(self):
         town_dashboard = (
-            f"In {self.name}'s town, there are currently {len(self.constructions)} constructions and {len(self.critters)} critters.\n"
+            f"\nIn {self.name}'s town, there are currently {len(self.constructions)} constructions and {len(self.critters)} critters.\n"
             f"It represents {self.total_points} Victory Points.\n"
             f"There are also {self.workers} workers and the following resources available:\n"
         )
 
         # This could be handled a different way through a dedicated global function maybe?
-        # Could then use it in every sentences... -_-
+        # Could then use it in every sentences... -_-;
         for resource, quantity in self.resources.items():
             if quantity > 1 and resource == "Berry":
                 town_dashboard += f"- {quantity} Berries\n"
@@ -57,15 +120,15 @@ class Town:
                 town_dashboard += f"- {quantity} {resource}\n"
 
         print(town_dashboard)
-        self.action_menu()
-        self.process_user_input()
+        self.game.action_menu()
+        self.game.process_user_input()
 
     # For this version of the game, each player will get cards only once
     def first_cards_gen(self, cards_data):
         # Deal 5 cards to each player
         for cards_count in range(5):
             if len(cards_data) > 0:
-                # Randomize and pop cards that get into players' hand
+                # Randomize and pop cards from deck
                 i = random.randint(0, len(cards_data)-1)
                 card = Card(
                         cards_data[i]["deck"],
@@ -80,40 +143,11 @@ class Town:
 
         # return cards_in_hand
 
-    # Display possible player's actions
-    def action_menu(self):
-        print("Choose an action:")
-        print("1. Display Town's Dashboard")
-        print("2. Display cards in hand")
-        print("3. Place a worker")
-
-    # Process player's choice
-    def process_user_input(self):
-        user_input = input(f"{self.name}, what do you wanna do next? ")
-        if user_input == "1":
-            self.town_dashboard()
-        elif user_input == "2":
-            if len(self.cards_in_hand) > 0:
-                print(f"{self.name}, here are the cards you have in hand:\n")
-                print(self.cards_in_hand)
-                self.play_card(input(f"{self.name}, which card do you wanna play? "))
-            else:
-                print(f"{self.name}, you don't have any cards in your hand.")
-                self.action_menu()
-                self.process_user_input()
-        elif user_input == "3":
-            self.place_worker()
-        else:
-            print(
-                "Your choice is not valid. Input the number of the action you wanna do."
-            )
-            self.action_menu()
-            self.process_user_input()
 
     # Check if played card valid, remove it from player's hand and add it to town
     def play_card(self, card):
         if card.lower() in self.cards_in_hand:
-            print(f"You just played {card.title()}.")
+            print(f"\n{self.name}, you just played {card.title()}.")
             played_card = self.cards_in_hand[card.lower()]
             # Checking type of card to add to the right category
             if played_card.deck == "critter":
@@ -122,13 +156,15 @@ class Town:
                 self.constructions.append(self.cards_in_hand.pop(card.lower()))
             # Adding card's points to Town's Victory Points
             self.total_points += played_card.points
-            print(f"You now have {len(self.critters)} critters and {len(self.constructions)} constructions in your town. It represents {self.total_points} Victory Points.")
+            print(f"You now have {len(self.constructions)} constructions and {len(self.critters)} critters in your town. It represents {self.total_points} Victory Points.")
         else:
             print("You don't have this card in your hand.")
-            self.play_card(input("Please choose an other card. "))
-        self.action_menu()
-        self.process_user_input()
+            self.play_card(input("Please choose another card: "))
+        self.game.switch_player()
+        self.game.action_menu()
+        self.game.process_user_input()
 
+# Generic class to handle any type of cards
 class Card:
     def __init__(self, deck, name, type, cost, points, unique=False):
         self.deck = deck
@@ -162,15 +198,14 @@ intro_logo = r"""
 print(intro_logo)
 player1_name = input("Ready to play Python Everdell?\nWhat is Player 1's name? ")
 player2_name = input(
-    "Welcome " + str(player1_name) + "!\nWhat is the Player 2's name? "
+    "\nWelcome " + str(player1_name) + "!\nWhat is the Player 2's name? "
 )
+current_game = Game(player1_name, player2_name)
 
-player1_town = Town(player1_name)
-player2_town = Town(player2_name)
+# current_game.action_menu()
+# current_game.process_user_input()
 
 
-player1_town.action_menu()
-player1_town.process_user_input()
 
 # Check deck length (uncomment for test only)
 # print("lenght of deck:" + str(len(cards_data)))
